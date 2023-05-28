fa85c9d29c486b2a000d5a4ef1c4f7435dcd6252
Jordan Masone <jordanmasone1@gmail.com>
2023-05-28 15:42:24 -0400

    Configured mongoDB connection

--- build/server.js
+++ build/server.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log("test");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Dependencies
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const morgan_1 = __importDefault(require("morgan"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+// Initilization
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || "3001";
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, morgan_1.default)("tiny"));
+app.get("/", (req, res) => {
+    res.send("Express and TypeScript Server");
+});
+app.listen(PORT, () => __awaiter(void 0, void 0, void 0, function* () {
+    const dbConnect = yield mongoose_1.default.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${dbConnect.connection.host}`);
+    console.log(`⚡️[server]: Server Listening at: http://localhost:${PORT}`);
+}));
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
-console.log("test");
+// Dependencies
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Initilization
+const app: Express = express();
+const PORT: string = process.env.PORT || "3001";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express and TypeScript Server");
+});
+
+app.listen(PORT, async () => {
+    const dbConnect = await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log(`MongoDB Connected: ${dbConnect.connection.host}`);
+    console.log(`⚡️[server]: Server Listening at: http://localhost:${PORT}`);
+});
